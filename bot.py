99ae0e0f51bda3f7f0a1c0af2b9839cdc8ec2033
AdityaGupta8429 <167102013+AdityaGupta8429@users.noreply.github.com>
2026-03-14 09:34:58 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,30 +1,29 @@
 from telethon import TelegramClient, events
 import requests
-import asyncio
 
-api_id = "28813625"
+api_id = 28813625
 api_hash = "792c04f8813f429d387ec93ba1a6332d"
 
 bot_token = "8597255898:AAGL4xY1V9LCQV13Gl7NwJNqXVUiLy2-054"
 chat_id = "7283933799"
 
-target = "+91 88648 82499"
+# phone number without spaces
+target_phone = "+918864882499"
 
 client = TelegramClient("session", api_id, api_hash)
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    data = {
+    requests.post(url, data={
         "chat_id": chat_id,
         "text": text
-    }
-    requests.post(url, data=data)
+    })
 
 @client.on(events.UserUpdate)
 async def handler(event):
     user = await event.get_user()
 
-    if user.username == target_username:
+    if user.phone and ("+" + user.phone) == target_phone:
 
         if event.online:
             print("User ONLINE")
@@ -34,10 +33,7 @@ async def handler(event):
             print("User OFFLINE")
             send_message("⚫ User went OFFLINE")
 
+print("Bot started...")
 
-async def main():
-    print("Bot started...")
-    await client.run_until_disconnected()
-
-client.start()
-asyncio.run(main())
+with client:
+    client.run_until_disconnected()
